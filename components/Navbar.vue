25ca06e0fd73ebf9fa3fccde29a95825dd9ed711
Admin <admin@AlanPacheco.local>
2023-10-03 22:31:32 -0700

    Added change color mode

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,20 +3,17 @@
     class="sticky top-0 z-20 w-full bg-opacity-80 backdrop-filter backdrop-blur bg-gradient-to-t from-transparent via-transparent to-white dark:to-black"
   >
     <nav class="md:flex md:items-center md:justify-between px-8 md:px-20 py-4">
-      <!-- <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
-      <UButton
-        color="white"
-        label="Options"
-        trailing-icon="i-heroicons-chevron-down-20-solid"
-      />
-    </UDropdown> -->
       <div class="flex items-center justify-between">
         <div>Logo</div>
-        <div class="md:hidden">
+
+        <div class="md:hidden flex">
+          <div class="mr-3">
+            <NavbarColorModePicker />
+          </div>
           <UButton
             color="gray"
             variant="ghost"
-            icon="i-heroicons-bars-3-solid"
+            icon="i-heroicons-bars-3-20-solid"
             @click="isOpen = true"
           />
           <USlideover v-model="isOpen" prevent-close>
@@ -58,40 +55,5 @@
   </header>
 </template>
 <script setup>
-const isTransparent = ref(false);
 const isOpen = ref(false);
-const items = [
-  [
-    {
-      label: "Light",
-      value: "light",
-      icon: "i-heroicons-sun-20-solid",
-      shortcuts: ["⌘", "L"],
-    },
-    {
-      label: "Dark",
-      value: "dark",
-      icon: "i-heroicons-moon-20-solid",
-      shortcuts: ["⌘", "D"],
-    },
-    {
-      label: "System",
-      value: "system",
-      icon: "i-heroicons-computer-desktop-20-solid",
-      shortcuts: ["⌘", "S"],
-    },
-  ],
-];
-
-onMounted(() => {
-  window.addEventListener("scroll", handleScroll);
-});
-
-const handleScroll = () => {
-  const scrollPosition =
-    window.pageYOffset || document.documentElement.scrollTop;
-  const threshold = 5; // Ajusta este valor según tus necesidades
-
-  isTransparent.value = scrollPosition > threshold;
-};
 </script>
---
+++ components/navbar/ColorModePicker.vue
@@ -0,0 +1,44 @@
+<template>
+  <UDropdown
+    v-model="selected"
+    :items="items"
+    :popper="{ placement: 'bottom-start' }"
+  >
+    <UButton
+      color="white"
+      trailing-icon="i-heroicons-adjustments-horizontal-solid"
+    />
+  </UDropdown>
+</template>
+<script setup>
+let colorMode = useColorMode();
+
+const items = [
+  [
+    {
+      label: "Light",
+      icon: "i-heroicons-sun-20-solid",
+      shortcuts: ["⌘", "L"],
+      click: () => {
+        colorMode.preference = "light";
+      },
+    },
+    {
+      label: "Dark",
+      icon: "i-heroicons-moon-20-solid",
+      shortcuts: ["⌘", "D"],
+      click: () => {
+        colorMode.preference = "dark";
+      },
+    },
+    {
+      label: "System",
+      icon: "i-heroicons-computer-desktop-20-solid",
+      shortcuts: ["⌘", "S"],
+      click: () => {
+        colorMode.preference = "system";
+      },
+    },
+  ],
+];
+</script>
