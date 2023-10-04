c429af7ba7cd1202c7a86d506b646c4a114da6b5
Admin <admin@AlanPacheco.local>
2023-10-03 19:23:04 -0700

    commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  target: "static",
+  ssr: false,
+  generate: {
+    fallback: true,
+  },
   devtools: {
     enabled: true,
 
